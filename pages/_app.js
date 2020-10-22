8e48a340d09e7a7847f49f4927b0bd36403c699e
dylan-albertazzi <albertad@oregonstate.edu>
2020-10-21 21:05:52 -0700

    add webclip

--- pages/_app.js
+++ pages/_app.js
@@ -30,6 +30,7 @@ function MyApp({ Component, pageProps }) {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👍</text></svg>"
         />
+        <link rel="apple-touch-icon" href="https://i.imgur.com/ows3z1X.png" />
 
         <script
           async
