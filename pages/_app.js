0d2ce1ac6d69c32884b139d22e7fb38615608e5a
dylan-albertazzi <albertad@oregonstate.edu>
2020-10-23 09:45:40 -0700

    google analytics take 2

--- pages/_app.js
+++ pages/_app.js
@@ -36,7 +36,7 @@ function MyApp({ Component, pageProps }) {
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-66J1QY576Q"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
+          function gtag(){window.dataLayer.push(arguments)}
           gtag('js', new Date());
 
           gtag('config', 'G-66J1QY576Q');
